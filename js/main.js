4952be7812a6beb82db393b58ba63af21296a76d
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-30 22:09:58 +0200

    Creating JS PART I - Animating Button Filter by when active and not active

--- js/main.js
+++ js/main.js
@@ -20,9 +20,15 @@ osomMovie.filterSlider = function() {
     $('.filter.open').on('click', function() {
         $('.filter_container').slideToggle(300, function() {
 
+            var btn = $(this).prev();
 
-
-
+            if(btn.hasClass('active')) {
+                $('.filter.open').find('.btn_title').text('Filter by');
+                btn.removeClass('active');
+            } else {
+                $('.filter.open').find('.btn_title').text('Close');
+                btn.addClass('active');
+            }
         });
     });
 
